c5a19e3335792970fccf14d5f991e8a4d2975e60
John <jonnygovish@gmail.com>
2017-10-19 18:10:23 +0300

    update models

---
+++ app/models.py
@@ -0,0 +1,11 @@
+
+class News_source:
+  """
+  News_source class that defines source Objects
+  """
+  def __init__(self, id, name,description, url, category):
+    self.id = id
+    self.name = name
+    self.description = description
+    self.url = url
+    self.category = category
