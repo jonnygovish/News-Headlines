13735e50d59ad06bf15dcf887f550ffc8e7aa3b4
John <jonnygovish@gmail.com>
2017-10-19 13:42:28 +0300

    add configurations for the application

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
   NEWS_SOURCE_API_BASE_URL = 'https://newsapi.org/v1/sources?language=en'
   NEWS_ARTICLE_BASE_URL =''
-  NEWS_API_KEY= os.environ,get('NEWS_API_KEY')
+  NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
   pass
