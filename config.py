113f1987849288a057fc01c8cb43082776d52de2
John <jonnygovish@gmail.com>
2017-10-19 18:09:54 +0300

    update  config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-  NEWS_SOURCE_API_BASE_URL = 'https://newsapi.org/v1/sources?language=en'
+  NEWS_SOURCE_API_BASE_URL = 'https://newsapi.org/v1/sources?category={}&language=en'
   NEWS_ARTICLE_BASE_URL =''
   NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
 
