76d64e3208f705deaa75790054b3ff20a7d3f96b
John <jonnygovish@gmail.com>
2017-10-21 07:30:50 +0300

    Add base url for articles

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
   NEWS_SOURCE_API_BASE_URL = 'https://newsapi.org/v1/sources?category={}&language=en'
-  NEWS_ARTICLE_BASE_URL =''
+  # NEWS_SOURCE_API_BASE_URL = 'https://newsapi.org/v1/sources?'
+  NEWS_ARTICLE_BASE_URL ='https://newsapi.org/v1/articles?source={}&sortBy=latest&apiKey={}'
   NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
