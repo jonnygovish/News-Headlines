e56ece053cf3ec64fefa7a6aff204ad971393733
John <jonnygovish@gmail.com>
2017-10-21 07:32:28 +0300

    Retrieving sources data

--- app/request.py
+++ app/request.py
@@ -14,5 +14,42 @@ def configure_request(app):
   news_source_url = app.config['NEWS_SOURCE_API_BASE_URL']
   article_url = app.config['NEWS_ARTICLE_BASE_URL']
 
+def get_source(category):
+  """
+  """
+  get_source_url = news_source_url.format(category)
+
+  with urllib.request.urlopen(get_source_url) as url:
+    get_source_data = url.read()
+    get_source_response = json.loads(get_source_data)
+
+    source_results = None
+
+    if get_source_response['sources']:
+      source_result_list =get_source_response['sources']
+
+      source_results = process_results(source_result_list)
+  
+  return source_results
+
+def process_results(source_list):
+  """
+  """
+  source_results = []
+
+  for source_item in source_list:
+    id = source_item.get('id')
+    name = source_item.get('name')
+    description = source_item.get('description')
+    url = source_item.get('url')
+    category = source_item.get('category')
+
+    source_object = News_source(id,name,description,url,category)
+    source_results.append(source_object)
+
+  return source_results
+
+  
+
 
 
