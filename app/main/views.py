5bdf16a7546ec1c6050aef5d97f0f7297c1c0c4b
John <jonnygovish@gmail.com>
2017-10-22 12:32:14 +0300

    Create a route for displaying  articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
-from ..request import get_source
+from ..request import get_source, get_article
 
 #views
 @main.route('/')
@@ -17,6 +17,17 @@ def index():
   politics_source = get_source('politics')
   music_source = get_source('music')
   sport_source = get_source('sport')
-  print(sport_source)
+  # print(sport_source)
   title = 'News Headlines'
   return render_template('index.html', title = title, sport = sport_source,business = business_source, entertainment = entertainment_source, general = general_source, gaming = gaming_source, technology = technology_source, politics = politics_source, music = music_source)
+
+@main.route('/source/<id>')
+def source(id):
+  """
+  """
+  article = get_article(id)
+  # print(article)
+  source_id = id
+  title = f'{source_id}'
+  return render_template('source.html',title = title,article = article)
+
