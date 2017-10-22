adc5b5fa565a543fb34d06ab9b991c7772481f89
John <jonnygovish@gmail.com>
2017-10-22 12:31:48 +0300

    Create request function for getting articles

--- app/request.py
+++ app/request.py
@@ -1,5 +1,5 @@
 import urllib.request,json
-from .models import News_source
+from .models import Source, Articles
 
 #api key
 api_key = None
@@ -44,10 +44,51 @@ def process_results(source_list):
     url = source_item.get('url')
     category = source_item.get('category')
 
-    source_object = News_source(id,name,description,url,category)
+    source_object = Source(id,name,description,url,category)
     source_results.append(source_object)
 
   return source_results
+
+def get_article(source):
+  get_article_url = article_url.format(source,api_key)
+  # get_article_url = 'https://newsapi.org/v1/articles?source=the-new-york-times&apiKey=e2ddbdf56d69495489a40f4d451bb2b8'
+  
+  with urllib.request.urlopen(get_article_url) as url:
+    get_article_data = url.read()
+    get_article_data_response = json.loads(get_article_data)
+
+    article_results = None
+
+    if get_article_data_response['articles']:
+      article_results_list = get_article_data_response['articles']
+      article_results = process_article_results(article_results_list)
+      # print(article_results)
+  # print(article_results)
+  return article_results
+
+def process_article_results(article_results):
+  """
+  """
+
+  article_list = []
+
+  for article_item in article_results:
+    author = article_item.get('author')
+    title = article_item.get('title')
+    description = article_item.get('description')
+    url = article_item.get('url')
+    picture = article_item.get('urlToImage')
+    date= article_item.get('publishedAt')
+
+    # if date:
+    article_object = Articles(author,title,description,url,picture,date)
+    print(article_object)
+    article_list.append(article_object)
+  
+  print(article_list)
+  return article_list
+
+      
 
   
 
