c8ca3284adef31bab2fe6b83219b93dfff54053f
John <jonnygovish@gmail.com>
2017-10-22 15:58:44 +0300

    test

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ manager.add_command('server', Server)
 def test():
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('test')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
--- test/test_articles.py
+++ test/test_articles.py
@@ -5,14 +5,14 @@ class TestArticle(unittest.TestCase):
   '''
 	Test Class to test the behaviour of the Article class
 	'''
-	def setUp(self):
-		'''
-		Set up that will run before every Test
-		'''
-		self.new_article = Article("Angela Medina","Redesigning the TechCrunch app,"Over the last two years we have been working hard to improve the experience of TechCrunch products for our readers",""https://techcrunch.com/2017/10/21/redesigning-the-techcrunch-app/"",""https://tctechcrunch2011.files.wordpress.com/2017/10/1-lead-image.jpg","2017-10-19T14:57:19Z")
+  def setUp(self):
+    '''
+    Set up that will run before every Test
+    '''
+    self.new_article = Articles("Angela Medina","Redesigning the TechCrunch app","Over the last two years we have been working hard to improve the experience of TechCrunch products for our readers","https://techcrunch.com/2017/10/21/redesigning-the-techcrunch-app/","https://tctechcrunch2011.files.wordpress.com/2017/10/1-lead-image.jpg","2017-10-21T11:00:53Z")
 
-	def test_isArticleInstance(self):
-		self.assertTrue(isinstance(self.new_article,Article))
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_article,Articles))
 		
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
